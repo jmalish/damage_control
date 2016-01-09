de58c5d61035308c8f9d19d8549a02b9dcb7725c
racer0940 <jordanmalish@gmail.com>
2016-01-09 13:09:19 -0500

    Add projectile shooting

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+    public int speed = 50;
+
+	// Use this for initialization
+	void Update () {
+
+        GetComponent<Rigidbody2D>().AddForce(transform.up * speed);
+    }
+	
+	void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SolidLaser.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserScript : MonoBehaviour {
+
+    LineRenderer line;
+
+	void Start()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+        line.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StopCoroutine("FireLaser");
+            StartCoroutine("FireLaser");
+        }
+    }
+
+    IEnumerator FireLaser()
+    {
+        line.enabled = true;
+        
+        while(Input.GetButton("Fire1"))
+        {
+            //line.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, Time.time);  // makes laser "rotate", gives it movement
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            line.SetPosition(0, ray.origin);
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                line.SetPosition(1, hit.point);
+                if (hit.rigidbody)
+                {
+                    hit.rigidbody.AddForceAtPosition(transform.forward * 5, hit.point);
+                }
+            } else
+            {
+                line.SetPosition(1, ray.GetPoint(1000));
+            }
+
+            line.SetPosition(1, ray.GetPoint(100));
+
+            yield return null;
+        }
+
+        line.enabled = false;
+    }
+}
